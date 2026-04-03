cc7d8a694eb12fd75ed9a8abe165055fe70365f7
anlikel <antongubenko2@gmail.com>
2026-04-03 14:28:49 +0300

    add backoff to feign

--- Project-10/feign/input/src/main/java/ru/astondevs/input/feign/OutputFeignClient.java
+++ Project-10/feign/input/src/main/java/ru/astondevs/input/feign/OutputFeignClient.java
@@ -2,9 +2,13 @@ package ru.astondevs.input.feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.astondevs.input.feign.configuration.FeignRetryConfig;
 import ru.astondevs.input.web.response.ApiResponse;
 
-@FeignClient(name = "output-client", url = "${output.service.url:http://localhost:8180}")
+@FeignClient(name = "output-client",
+    url = "${output.service.url:http://localhost:8180}",
+    configuration = FeignRetryConfig.class
+)
 public interface OutputFeignClient {
 
     @GetMapping("/output/rest")
---
+++ Project-10/feign/input/src/main/java/ru/astondevs/input/feign/configuration/FeignRetryConfig.java
@@ -0,0 +1,14 @@
+package ru.astondevs.input.feign.configuration;
+
+import feign.Retryer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignRetryConfig {
+
+    @Bean
+    public Retryer feignRetryer() {
+        return new Retryer.Default(1000, 5000, 3);
+    }
+}
